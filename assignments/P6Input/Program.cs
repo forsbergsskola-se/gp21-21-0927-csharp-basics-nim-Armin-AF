b3dab8d36c2941a10fe094906035585b7d44d712
Armin Afazeli <armin.afazeli@student.forsbergsskola.se>
2021-09-28 13:56:29 +0200

    6 - Reading Input

---
+++ assignments/P6Input/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace P6Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Nice to meet you {name}!");
+        }
+    }
+}
