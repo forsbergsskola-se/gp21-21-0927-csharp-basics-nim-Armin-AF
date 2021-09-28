60d313c0f0d5412396380e605e1c9030372522f4
Armin Afazeli <armin.afazeli@student.forsbergsskola.se>
2021-09-28 12:35:04 +0200

    2 - Comments:

---
+++ assignments/P2Comments/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Author: Armin Afazeli
+   Date:   2021-09-28
+   E-mail: armin.afazeli@Hotmail.com */
+
+using System;
+
+namespace P2Comments
+{
+    class Program
+    {
+        /// <Main method>
+        /// It means Main Method can be called without an object.
+        /// </The Main method doesn’t return anything.>
+        
+        static void Main(string[] args)
+        {
+            // Writes the text representation of the specified objects, followed by the current line terminator, to the standard output stream using the specified format information.
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
