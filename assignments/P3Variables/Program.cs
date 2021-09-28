2dac1905f0fd284c42c3b8de4179df8872f151ac
Armin Afazeli <armin.afazeli@student.forsbergsskola.se>
2021-09-28 13:09:43 +0200

    3 - Variables:

---
+++ assignments/P3Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace P3Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            Console.WriteLine($"The number is {number}.");
+        }
+    }
+}
