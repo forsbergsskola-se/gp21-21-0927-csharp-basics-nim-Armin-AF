de624f92376ac8e280bc2b0ec95f8343ad73b831
Armin Afazeli <armin.afazeli@student.forsbergsskola.se>
2021-09-28 13:21:40 +0200

    4 - Basic Data Types:

---
+++ assignments/P4DataTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P4DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string outputString = "Hello World!";
+            Console.WriteLine(outputString);
+            float decimalVaue = 0.3f;
+            Console.WriteLine(decimalVaue);
+            bool isAwesome = true;
+            Console.WriteLine(isAwesome);
+            byte hexByte = 0xa7;
+            Console.WriteLine(hexByte);
+        }
+    }
+}
