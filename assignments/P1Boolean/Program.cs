17bacffcc9d3531b54cdae69d56a17fd48aa068f
Armin Afazeli <armin.afazeli@student.forsbergsskola.se>
2021-09-29 11:14:35 +0200

    10. Boolean

---
+++ assignments/P1Boolean/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics.SymbolStore;
+
+namespace P1Boolean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            bool isChild = age > 0 && age < 12;
+            bool isTeenager = age > 13 && age < 19;
+            bool isGrownup = age > 19;
+            Console.WriteLine("You are a child: " + isChild);
+            Console.WriteLine("You are a teenager: " + isTeenager);
+            Console.WriteLine("You are a grown-up: " + isGrownup);
+
+
+        }
+            
+    }
+}
