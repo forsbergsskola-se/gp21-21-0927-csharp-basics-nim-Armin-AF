9cfb97ac9062b156a8a5ff9b203ac17698e085a3
Armin Afazeli <armin.afazeli@student.forsbergsskola.se>
2021-09-29 11:23:58 +0200

    11. If .. Else

---
+++ assignments/P11IfElse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace P11IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age > 0 && age < 12)
+            {
+                Console.WriteLine("You are a child!");
+            } 
+            else if(age <19)
+            {
+                Console.WriteLine("You are a teenager!");
+            }
+            else
+            {
+                Console.WriteLine("You are a grown-up!");
+            }
+        }
+    }
+}
