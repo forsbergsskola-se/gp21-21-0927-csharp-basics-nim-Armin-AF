ab3108c68d439892d6f50bb50636a128696e5452
Armin Afazeli <armin.afazeli@student.forsbergsskola.se>
2021-09-30 09:01:01 +0200

    Tic Tac Toe Start

---
+++ assignments/TicTacToe/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Greeting
+            Console.WriteLine("Hello! Welcome to the Tic Tac Toe Game!");
+            
+            // Displaying the players
+            Console.WriteLine("Player 1: x");
+            Console.WriteLine("Player 2: o");
+            
+            
+            // Turn to move
+            Console.WriteLine("It is player 1 ( x ) turn. Select a number between 1 1nd 9:");
+            
+            
+            // The display
+
+            Console.WriteLine(" 1 | 2 | 3 ");
+            Console.WriteLine("---+---+---");
+            Console.WriteLine(" 4 | 5 | 6 ");
+            Console.WriteLine("---+---+---");
+            Console.WriteLine(" 7 | 8 | 9 ");
+            
+            // Player input
+
+            string playerInput = Console.ReadLine();
+
+
+
+        }
+    }
+}
