45286d4ad863a4e3b6ab3d3b86574c02029fa255
Armin Afazeli <armin.afazeli@student.forsbergsskola.se>
2021-09-30 13:27:19 +0200

    Nim game Updated
    String entry error solved!

--- assignments/Nim/Program.cs
+++ assignments/Nim/Program.cs
@@ -7,16 +7,38 @@ namespace Nim
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to nim!");
+            
+            Console.WriteLine(@"          
+          _____                    _____                    _____                            _____                    _____                    _____                    _____          
+         /\    \                  /\    \                  /\    \                          /\    \                  /\    \                  /\    \                  /\    \         
+        /::\____\                /::\    \                /::\____\                        /::\    \                /::\    \                /::\____\                /::\    \        
+       /::::|   |                \:::\    \              /::::|   |                       /::::\    \              /::::\    \              /::::|   |               /::::\    \       
+      /:::::|   |                 \:::\    \            /:::::|   |                      /::::::\    \            /::::::\    \            /:::::|   |              /::::::\    \      
+     /::::::|   |                  \:::\    \          /::::::|   |                     /:::/\:::\    \          /:::/\:::\    \          /::::::|   |             /:::/\:::\    \     
+    /:::/|::|   |                   \:::\    \        /:::/|::|   |                    /:::/  \:::\    \        /:::/__\:::\    \        /:::/|::|   |            /:::/__\:::\    \    
+   /:::/ |::|   |                   /::::\    \      /:::/ |::|   |                   /:::/    \:::\    \      /::::\   \:::\    \      /:::/ |::|   |           /::::\   \:::\    \   
+  /:::/  |::|   | _____    ____    /::::::\    \    /:::/  |::|___|______            /:::/    / \:::\    \    /::::::\   \:::\    \    /:::/  |::|___|______    /::::::\   \:::\    \  
+ /:::/   |::|   |/\    \  /\   \  /:::/\:::\    \  /:::/   |::::::::\    \          /:::/    /   \:::\ ___\  /:::/\:::\   \:::\    \  /:::/   |::::::::\    \  /:::/\:::\   \:::\    \ 
+/:: /    |::|   /::\____\/::\   \/:::/  \:::\____\/:::/    |:::::::::\____\        /:::/____/  ___\:::|    |/:::/  \:::\   \:::\____\/:::/    |:::::::::\____\/:::/__\:::\   \:::\____\
+\::/    /|::|  /:::/    /\:::\  /:::/    \::/    /\::/    / ~~~~~/:::/    /        \:::\    \ /\  /:::|____|\::/    \:::\  /:::/    /\::/    / ~~~~~/:::/    /\:::\   \:::\   \::/    /
+ \/____/ |::| /:::/    /  \:::\/:::/    / \/____/  \/____/      /:::/    /          \:::\    /::\ \::/    /  \/____/ \:::\/:::/    /  \/____/      /:::/    /  \:::\   \:::\   \/____/ 
+         |::|/:::/    /    \::::::/    /                       /:::/    /            \:::\   \:::\ \/____/            \::::::/    /               /:::/    /    \:::\   \:::\    \     
+         |::::::/    /      \::::/____/                       /:::/    /              \:::\   \:::\____\               \::::/    /               /:::/    /      \:::\   \:::\____\    
+         |:::::/    /        \:::\    \                      /:::/    /                \:::\  /:::/    /               /:::/    /               /:::/    /        \:::\   \::/    /    
+         |::::/    /          \:::\    \                    /:::/    /                  \:::\/:::/    /               /:::/    /               /:::/    /          \:::\   \/____/     
+         /:::/    /            \:::\    \                  /:::/    /                    \::::::/    /               /:::/    /               /:::/    /            \:::\    \         
+        /:::/    /              \:::\____\                /:::/    /                      \::::/    /               /:::/    /               /:::/    /              \:::\____\        
+        \::/    /                \::/    /                \::/    /                        \::/____/                \::/    /                \::/    /                \::/    /        
+         \/____/                  \/____/                  \/____/                                                   \/____/                  \/____/                  \/____/         
+                                                                                                                                                                                       ");
             Console.WriteLine("RULES: ");
             Console.WriteLine("The game begins with placing 24 matches in a pile.");
             Console.WriteLine("Players take turns to draw matches");
             Console.WriteLine("Each player may draw 1,2 or 3 matches (not more or less).");
             Console.WriteLine("The Player who has to take last match wins!");
-            
+            Console.WriteLine("  ");
             
             int matches = 24;
-            
             
             while (matches > 0)
             {
@@ -25,25 +47,44 @@ namespace Nim
                     Console.Write("|");
                 }
                 Console.WriteLine( "  " + matches);
-                Console.WriteLine("How many matches would do you like to draw?");
-                int playerPick = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("How many matches would you like to draw?");
                 
+                int playerPick;
+                string playerPickString = Console.ReadLine();
+                bool errorCheck = int.TryParse(playerPickString, out playerPick);
 
-                if (playerPick > 3 | playerPick < 1)
-                { 
-                    Console.WriteLine("You can pick 1, 2 or 3 matches!(Input 1, 2 or 3) try again!");
+                if (errorCheck)
+                {
+                    if (playerPick > 3 | playerPick < 1)
+                    { 
+                        Console.WriteLine("You can pick 1, 2 or 3 matches!(Input 1, 2 or 3) try again!");
+                    }
+                    else
+                    {
+                        matches = matches - playerPick;
+                        Console.WriteLine("AI picks: " + (4-playerPick) + "!");
+                        matches = matches - (4 - playerPick);
+                    }
                 }
                 else
                 {
-                    matches = matches - playerPick;
-                    Console.WriteLine("AI picks: " + (4-playerPick) + "!");
-                    matches = matches - (4 - playerPick);
-                    
-                    
+                    Console.WriteLine("Invalid input! Type 1,2 or 3.");
                 }
+                
+
             }
 
-            Console.WriteLine("AI win! Better luck next time! Probably not :))))) ");
+            Console.WriteLine(@"                                                                                                                                                                                                                                     
+ __    __  _____   __  __         __       _____   ____    ____      
+/\ \  /\ \/\  __`\/\ \/\ \       /\ \     /\  __`\/\  _`\ /\  _`\    
+\ `\`\\/'/\ \ \/\ \ \ \ \ \      \ \ \    \ \ \/\ \ \,\L\_\ \ \L\_\  
+ `\ `\ /'  \ \ \ \ \ \ \ \ \      \ \ \  __\ \ \ \ \/_\__ \\ \  _\L  
+   `\ \ \   \ \ \_\ \ \ \_\ \      \ \ \L\ \\ \ \_\ \/\ \L\ \ \ \L\ \
+     \ \_\   \ \_____\ \_____\      \ \____/ \ \_____\ `\____\ \____/
+      \/_/    \/_____/\/_____/       \/___/   \/_____/\/_____/\/___/ 
+                                                                     
+                                                                      ");
+            Console.WriteLine("Better luck next time! Probably not :))))) ");
             Console.ReadLine();
         }
     }
